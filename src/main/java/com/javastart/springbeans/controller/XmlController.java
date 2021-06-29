b674268e012aaac5a0a90f640597f0d71d4bbbb7
Alexey <groznovaa@gmail.comls>
2021-06-29 18:47:45 +0500

    Add package contorller and XmlController class and method helloXml

--- src/main/java/com/javastart/springbeans/controller/XmlController.java
+++ src/main/java/com/javastart/springbeans/controller/XmlController.java
@@ -1,4 +1,13 @@
 package com.javastart.springbeans.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class XmlController {
+
+    @GetMapping("/xml")
+    public String helloXml(){
+
+    }
 }
