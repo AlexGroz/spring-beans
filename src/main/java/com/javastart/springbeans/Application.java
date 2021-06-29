8095660990b6e598dae758adb8cf7d18f05b0a10
Alexey <groznovaa@gmail.comls>
2021-06-29 18:58:32 +0500

    Add bean to application-context.xml

--- src/main/java/com/javastart/springbeans/Application.java
+++ src/main/java/com/javastart/springbeans/Application.java
@@ -2,8 +2,10 @@ package com.javastart.springbeans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:application-context.xml")
 public class Application {
 
     public static void main(String[] args) {
