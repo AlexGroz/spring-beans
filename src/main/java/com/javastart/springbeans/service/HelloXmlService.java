3eb019d3993f244edfa86876a9dca01ebe4ef047
Alexey <groznovaa@gmail.comls>
2021-06-29 20:30:03 +0500

    Add UtilsService variable, method setUtilService

--- src/main/java/com/javastart/springbeans/service/HelloXmlService.java
+++ src/main/java/com/javastart/springbeans/service/HelloXmlService.java
@@ -1,7 +1,14 @@
 package com.javastart.springbeans.service;
 
 public class HelloXmlService {
+
+    private UtilsService utilsService;
+
     public String hello(){
         return "Hello from xml!";
+    }
+
+    public void setUtilsService(UtilsService utilsService) {
+        this.utilsService = utilsService;
     }
 }
