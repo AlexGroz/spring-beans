bcfd632f7d1b573d0e2173f564dfba33b101f883
Alexey <groznovaa@gmail.comls>
2021-06-29 18:38:17 +0500

    Add package com.javastart.sprinbeans and main class Application

---
+++ src/main/java/com/javastart/springbeans/Application.java
@@ -0,0 +1,11 @@
+package com.javastart.springbeans;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
