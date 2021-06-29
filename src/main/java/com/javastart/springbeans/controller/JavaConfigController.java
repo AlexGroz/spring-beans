caf0af5180088957145c9a1d23eba4e4502f70c2
Alexey <groznovaa@gmail.comls>
2021-06-29 20:43:50 +0500

    Add method hello in JavaConfigController classs

--- src/main/java/com/javastart/springbeans/controller/JavaConfigController.java
+++ src/main/java/com/javastart/springbeans/controller/JavaConfigController.java
@@ -1,4 +1,13 @@
 package com.javastart.springbeans.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JavaConfigController {
+
+    @GetMapping("/java")
+    public String hello(){
+
+    }
 }
