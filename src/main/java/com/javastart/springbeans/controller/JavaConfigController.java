1bc13bcec6059765375cc7b8a124858f977fc199
Alexey <groznovaa@gmail.comls>
2021-06-29 20:51:40 +0500

    added value javaConfigService and method JavaConfigController to JavaConfigController

--- src/main/java/com/javastart/springbeans/controller/JavaConfigController.java
+++ src/main/java/com/javastart/springbeans/controller/JavaConfigController.java
@@ -1,10 +1,19 @@
 package com.javastart.springbeans.controller;
 
+import com.javastart.springbeans.service.JavaConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JavaConfigController {
+
+    private final JavaConfigService javaConfigService;
+
+    @Autowired
+    public JavaConfigController(JavaConfigService javaConfigService){
+        this.javaConfigService = javaConfigService;
+    }
 
     @GetMapping("/java")
     public String hello(){
